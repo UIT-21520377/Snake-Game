25646fd37f82cf1b101da9124e7f77d1a115bf72
UIT.21520377 <21520377@gm.uit.edu.vn>
2024-11-16 23:26:50 +0700

    modify header

--- Smake/Cursor.cpp
+++ Smake/Cursor.cpp
@@ -1,34 +1,5 @@
-#include <windows.h>
-class SnakeGame
-{
-private:
-    bool gameOver;
-    const int width = 60;
-    const int height = 25;
-    int headX, headY, fruitX, fruitY, score, highScore;
-    int bodyX[100], bodyY[100];
-    int lenSnake;
-    enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-    eDirection dir;
-    HANDLE hConsole;
-    bool isHardMode;
-    const int numBlocks = 10;
-    int blockX[10], blockY[10];
+#include "Header.h"
 
-public:
-    SnakeGame();
-    void HideCursor();
-    void ShowCursor();
-    void DrawBackground();
-    void DrawStartScreen();
-    void StartGame();
-    void Draw();
-    void Input();
-    void Logic();
-    void RunGame();
-    void ResetGame();
-    void GenerateBlocks();
-};
 void SnakeGame::HideCursor()
 {
     CONSOLE_CURSOR_INFO info;
--- Smake/SnakeGame.cpp
+++ Smake/SnakeGame.cpp
@@ -1,51 +1,6 @@
 #include <ctime>
-#include <windows.h>
-class SnakeGame
-{
-private:
-    bool gameOver;
-    const int width = 60;
-    const int height = 25;
-    int headX, headY, fruitX, fruitY, score, highScore;
-    int bodyX[100], bodyY[100];
-    int lenSnake;
-    enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-    eDirection dir;
-    HANDLE hConsole;
-    bool isHardMode;
-    const int numBlocks = 10;
-    int blockX[10], blockY[10];
+#include "Header.h"
 
-public:
-    SnakeGame();
-    void HideCursor();
-    void ShowCursor();
-    void DrawBackground();
-    void DrawStartScreen();
-    void StartGame();
-    void Draw();
-    void Input();
-    void Logic();
-    void RunGame();
-    void ResetGame();
-    void GenerateBlocks();
-};
-
-/*
-SnakeGame::SnakeGame()
-{
-    gameOver = false;
-    dir = STOP;
-    headX = width / 2;
-    headY = height / 2;
-    fruitX = rand() % width;
-    fruitY = rand() % height;
-    score = 0;
-    highScore = 0;
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    HideCursor();
-}
-*/
 SnakeGame::SnakeGame() {
     gameOver = false;
     dir = STOP;
