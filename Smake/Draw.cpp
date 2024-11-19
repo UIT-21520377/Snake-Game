efe074d66d4a219d36f05057ecda291260217000
UIT23520005 <23520005@gm.uit.edu.vn>
2024-11-19 17:17:11 +0700

    Update Draw.cpp

--- Smake/Draw.cpp
+++ Smake/Draw.cpp
@@ -29,22 +29,23 @@ void Snake::Rules()
 void Snake::MainScreen()
 {
     Rules();
-    cout << "              /^\\               \n";
-    cout << "             /   \\              \n";
-    cout << "        ____/_____\\____         \n";
-    cout << "       /              \\         \n";
-    cout << "      /  \\   O   O   / \\        \n";
-    cout << "     |    \\    ^    /   |       \n";
-    cout << "      \\    \\_______/   /        \n";
-    cout << "       \\                /        \n";
-    cout << "        \\___/~~~~~\\___/         \n";
-    cout << "          /           \\          \n";
-    cout << "         /             \\         \n";
-    cout << "     ___/               \\___     \n";
-    cout << "    /                       \\    \n";
-    cout << "   /                         \\   \n";
-    cout << "  /                           \\  \n";
-    cout << " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
+    // In ra hình minh họa rắn (một chú rắn vẽ ASCII).
+   cout << "              /^\\               \n";
+   cout << "             /   \\              \n";
+   cout << "        ____/_____\\____         \n";
+   cout << "       /              \\         \n";
+   cout << "      /  \\   O   O   / \\        \n";
+   cout << "     |    \\    ^    /   |       \n";
+   cout << "      \\    \\_______/   /        \n";
+   cout << "       \\                /        \n";
+   cout << "        \\___/~~~~~\\___/         \n";
+   cout << "          /           \\          \n";
+   cout << "         /             \\         \n";
+   cout << "     ___/               \\___     \n";
+   cout << "    /                       \\    \n";
+   cout << "   /                         \\   \n";
+   cout << "  /                           \\  \n";
+   cout << " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); 
 
 
