d36f6d27b2d290146b57eccd2f1e04f31a69d619
UIT-23521396 <23521396@gm.uit.edu.vn>
2024-11-19 14:56:58 +0700

    Update run.cpp

--- Smake/run.cpp
+++ Smake/run.cpp
@@ -16,3 +16,18 @@ void Snake::Run() {
 
     cout << "Game Over! Score: " << score << endl;
 }
+Snake::Snake() {
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    srand(static_cast<unsigned>(time(0)));
+    gameOver = false;
+    dir = STOP;
+    headX = width / 2;
+    headY = height / 2;
+    dollarX = rand() % width;
+    dollarY = rand() % height;
+    score = 0;
+    highScore = 0;
+    lenSnake = 0;
+    isHardMode = false;
+    HideCursor();
+}
