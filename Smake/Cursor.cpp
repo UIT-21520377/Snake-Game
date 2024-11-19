c117777f86092930282aa1485b89bf8a776572d9
luathuu <23520901@gm.uit.edu.vn>
2024-11-19 21:50:27 +0700

    Update Cursor.cpp

--- Smake/Cursor.cpp
+++ Smake/Cursor.cpp
@@ -2,16 +2,16 @@
 
 void Snake::HideCursor()
 {
-    CONSOLE_CURSOR_INFO info;
-    info.dwSize = 100;
-    info.bVisible = FALSE;
-    SetConsoleCursorInfo(hConsole, &info);
+    CONSOLE_CURSOR_INFO info; // Tạo cấu trúc lưu thông tin về con trỏ chuột.
+    info.dwSize = 100;  // Đặt kích thước con trỏ chuột (chỉ có ý nghĩa khi hiển thị).
+    info.bVisible = FALSE; // Thiết lập trạng thái ẩn con trỏ chuột.
+    SetConsoleCursorInfo(hConsole, &info); // Áp dụng thiết lập con trỏ cho console hiện tại.
 }
 
 void Snake::ShowCursor()
 {
-    CONSOLE_CURSOR_INFO info;
-    info.dwSize = 10;
-    info.bVisible = TRUE;
-    SetConsoleCursorInfo(hConsole, &info);
+    CONSOLE_CURSOR_INFO info; // Tạo cấu trúc lưu thông tin về con trỏ chuột.
+    info.dwSize = 10; // Đặt kích thước con trỏ chuột nhỏ hơn (chỉ để hiển thị gọn).
+    info.bVisible = TRUE; // Thiết lập trạng thái hiển thị con trỏ chuột.
+    SetConsoleCursorInfo(hConsole, &info); // Áp dụng thiết lập con trỏ cho console hiện tại.
 }
