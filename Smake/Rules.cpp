8489249bc44c76dac921befe6d76928683ccf2f1
UIT-23521396 <23521396@gm.uit.edu.vn>
2024-11-22 15:08:17 +0700

    Update Rules.cpp

--- Smake/Rules.cpp
+++ Smake/Rules.cpp
@@ -5,18 +5,18 @@ void Snake::Rules() {
     SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << "SIMPLE MODE:" << endl;
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-    cout << " - The snake moves at a slow speed." << endl;
-    cout << " - No obstacles in the arena." << endl;
-    cout << " - Boundaries are non-lethal (teleports to the opposite side)." << endl;
+    cout << " - The snake moves at a slow speed.- con rắn di chuyển với tốc độ chậm " << endl;
+    cout << " - No obstacles in the arena. -Không có chướng ngại vật trên đấu trường " << endl;
+    cout << " - Boundaries are non-lethal (teleports to the opposite side) - không có vật cản trên trường đấu tranh" << endl;
 
     cout << endl;
 
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
     cout << "HARD MODE:" << endl;
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-    cout << " - The snake moves much faster." << endl;
-    cout << " - Obstacles must be avoided." << endl;
-    cout << " - Colliding with boundaries or obstacles ends the game." << endl;
+    cout << " - The snake moves much faster. -Con rắn di chuyển nhanh hơn nhiều " << endl;
+    cout << " - Obstacles must be avoided.-Những trở ngại phải được tránh." << endl;
+    cout << " - Colliding with boundaries or obstacles ends the game.-Va chạm với ranh giới hoặc chướng ngại vật kết thúc trò chơi " << endl;
 
     cout << endl;
 }
